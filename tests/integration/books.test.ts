b68b7df138887fb6af0133d1d51858f681bb8719
Tiago Sousa <tiago.costa.sousa@proton.me>
2025-09-01 16:46:19 +0100

    test(books): change book search test to also check for improper 404 responses

--- tests/integration/books.test.ts
+++ tests/integration/books.test.ts
@@ -899,6 +899,13 @@ describe('Search books JWT', () => {
     let searchResponse = await searchBooks('non-existent', undefined, undefined, undefined, undefined, { jwt: registerResponse.body.jwt_token });
     expect(searchResponse.status).toBe(404);
     expect(searchResponse.text).toBe(USER_NOT_FOUND);
+
+    const { response: registerResponse2 } = await registerUser();
+    expect(registerResponse2.status).toBe(200);
+
+    searchResponse = await searchBooks('non-existent', undefined, undefined, undefined, undefined, { jwt: registerResponse2.body.jwt_token });
+    expect(searchResponse.status).toBe(403);
+    expect(searchResponse.text).toBe(FORBIDDEN);
   });
 
   test('Different user without permission', async () => {
